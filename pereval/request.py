ee1fc1a29cfeb992a0cf7e834899e11c6683eec4
Timur <marteen.free@gmail.com>
2024-04-13 20:48:51 +0300

    рабочий тестовый файл request

--- pereval/request.py
+++ pereval/request.py
@@ -2,37 +2,32 @@ import requests
 
 # Укажите данные для вашего нового перевала
 data = {
-    "beauty_title": "пер. ",
-    "title": "Название перевала",
-    "other_titles": "Дополнительные названия",
-    "connect": "Что соединяет",
-    "user": {
-        "email": "example@mail.com",
-        "fam": "Иванов",
-        "name": "Иван",
-        "otc": "Иванович",
-        "phone": "+71234567890"
-    },
-    "coords": {
-        "latitude": "55.1234",
-        "longitude": "37.5678",
-        "height": "1500"
-    },
-    "level": {
-        "winter": "",
-        "summer": "1А",
-        "autumn": "1А",
-        "spring": ""
-    },
-    "images": [
-        {"data": "<картинка1>", "title": "Описание 1"},
-        {"data": "<картинка2>", "title": "Описание 2"}
-    ]
+    "raw_data": {"beauty_title": "per. ",
+                 "title": "name",
+                 "other_titles": "name2",
+                 "connect": "con",
+                 "coords": {
+                     "latitude": "55.1234",
+                     "longitude": "37.5678",
+                     "height": "1500"},
+                 "level": {
+                     "winter": "",
+                     "summer": "1А",
+                     "autumn": "1А",
+                     "spring": ""}
+                 },
+    "images": [{"id": 1, "title": "Седловина"},
+               {"id": 2, "title": "Подъем"}
+               ],
+    'email': "example@mail.com",
+    'fam': "Ivanov",
+    "name": "Ivan",
+    "otc": "Ivanovich",
+    "phone": "+71234567890"
 }
 
 # Отправляем POST-запрос на наш эндпоинт submitData
-response = requests.post("http://127.0.0.1:8080/submitData", json=data)
-
+response = requests.post("http://127.0.0.1:5000/submitData", json=data)
 # Печатаем ответ
 print(response.status_code)
 print(response.json())
